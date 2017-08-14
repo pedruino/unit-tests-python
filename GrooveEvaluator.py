21f071b821b8ed0888f668ab348e7d085698ff99
pedruino <pedro.escobar@live.com>
2017-08-14 01:14:11 -0300

    changes

--- GrooveEvaluator.py
+++ GrooveEvaluator.py
@@ -36,12 +36,13 @@ class Magic:
 
         return Magic.__calculate_passes(material_to_remove, tool_thickness, top_left, top_right, bottom_left,
                                         bottom_right,
-                                        is_horizontal, pass_offset, pass_offset_interlock)
+                                        is_horizontal, pass_offset, pass_offset_interlock, 0)
 
     @staticmethod
     def __calculate_passes(material_to_remove, tool_thickness, top_left, top_right, bottom_left, bottom_right,
                            is_horizontal,
-                           pass_offset, pass_offset_interlock):
+                           pass_offset, pass_offset_interlock,
+                           material_removed):
         half_tool_thickness = tool_thickness / 2.0
         lines = []
 
@@ -54,19 +55,31 @@ class Magic:
         bottom_right += pass_offset
 
         lines.append(Line(bottom_left, bottom_right))
+        material_removed += tool_thickness
 
         if is_horizontal:
             material_to_remove = (top_left.y - bottom_left.y)
         else:
             material_to_remove = (top_left.X - bottom_left.X)
 
-        if material_to_remove <= 0:
+        if material_removed == half_tool_thickness > material_to_remove:
+            lines.append(Line(top_left, top_right))
             return lines
+        elif material_to_remove == 0:
+            return lines
+
+        if is_horizontal:
+            if material_to_remove < pass_offset_interlock.y:
+                return lines
+        else:
+            if material_to_remove < pass_offset_interlock.x:
+                return lines
 
         lines.append(Line(top_left, top_right))
+        material_removed += tool_thickness
 
-        if material_to_remove < half_tool_thickness:
-            return lines
+        # if material_to_remove < half_tool_thickness:
+        #    return lines
 
         top_left -= pass_offset
         top_right -= pass_offset
@@ -78,6 +91,8 @@ class Magic:
         bottom_left -= pass_offset_interlock
         bottom_right -= pass_offset_interlock
 
+        # material_removed -= tool_thickness
+
         if is_horizontal:
             material_to_remove = (top_left.y - bottom_left.y)
         else:
@@ -85,6 +100,6 @@ class Magic:
 
         lines.extend(Magic.__calculate_passes(material_to_remove, tool_thickness,
                                               top_left, top_right, bottom_left, bottom_right,
-                                              is_horizontal, pass_offset, pass_offset_interlock))
+                                              is_horizontal, pass_offset, pass_offset_interlock, material_removed))
 
         return lines
